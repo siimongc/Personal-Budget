afd24403d2cebb54cff5c61e8653ba5f57468980
siimongc <siimongaviria30@gmail.com>
2026-04-09 15:03:42 -0700

    dfsdasd

--- budget-app/src/lib/supabase.ts
+++ budget-app/src/lib/supabase.ts
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 // Usamos variables de entorno que el usuario proveerá después en un .env.local
 // Para la versión inicial y mockeos locales asumiendo que no tiene configurado nada aún
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://fake-project.supabase.co';
-const supabaseKey = import.meta.env.SUPABASE_ANON || 'fake-anon-key';
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON || 'fake-anon-key';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
